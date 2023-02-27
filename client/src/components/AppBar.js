e3c5609f70bcf99dbcad255b1f1aa5680dad8728
Lin Qiu <73025661+linqiu0-0@users.noreply.github.com>
2023-02-26 23:59:28 -0800

    Allow user edit profile (#34)
    * feat: profile page completed
    
    * fix: readme
    
    * fix merge issue
    
    * update avatar
    
    * frontend sublease info implemented
    
    * feat: update view profile
    
    * fix: remove add proper type check
    
    * feat: edit profile
    
    * Merge Frontend profile page (#26)
    
    * Add files via upload
    
    * changed week 7 report
    
    * Update DeveloperGuide.md
    
    * Add files via upload
    
    * Update DeveloperGuide.md
    
    * feat: profile page completed
    
    * fix: readme
    
    * fix merge issue
    
    * update avatar
    
    * feat: update view profile
    
    * fix: remove add proper type check
    
    * feat: edit profile
    
    ---------
    
    Co-authored-by: Tony Song <72170050+icytailovo@users.noreply.github.com>
    Co-authored-by: Zhinuo <64811271+zhinuoli@users.noreply.github.com>
    Co-authored-by: RyanLi0802 <lansong@cs.washington.edu>
    Co-authored-by: Ryan Li <71858089+RyanLi0802@users.noreply.github.com>
    
    * add account menu
    
    * feat: connect profile with backend
    
    * change router
    
    ---------
    
    Co-authored-by: icytail <icytailovo@gmail.com>
    Co-authored-by: Tony Song <72170050+icytailovo@users.noreply.github.com>
    Co-authored-by: Zhinuo <64811271+zhinuoli@users.noreply.github.com>
    Co-authored-by: RyanLi0802 <lansong@cs.washington.edu>
    Co-authored-by: Ryan Li <71858089+RyanLi0802@users.noreply.github.com>

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -3,19 +3,18 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import {createTheme, SvgIcon} from "@mui/material";
 import { ReactComponent as Logo } from '../assets/images/DubLeaseLogo.svg';
 import {ThemeProvider} from "@emotion/react";
 import { useNavigate } from "react-router-dom";
-
 import {AccountMenu} from "./AccountMenu"
 
 const pages = ['Home', 'Message', 'Add Lease', 'About Us'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-function MainAppBar({ username }) {
+function MainAppBar({ username, userId }) {
+    console.log("app bar" + userId)
     const [anchorElNav, setAnchorElNav] = React.useState(null);
     const [anchorElUser, setAnchorElUser] = React.useState(null);
     const navigate = useNavigate();
@@ -34,6 +33,8 @@ function MainAppBar({ username }) {
     const handleCloseUserMenu = () => {
         setAnchorElUser(null);
     };
+
+
 
 
 
@@ -74,9 +75,10 @@ function MainAppBar({ username }) {
 
                         <Box sx={{ flexGrow: 0 }}>
                             {/* <Button variant="contained" sx={{mr: 3}}  onClick={handleViewProfile}>View Profile</Button> */}
+                            {/* <Button variant="contained" sx={{mr: 3}}  onClick={handleViewProfile}>View Profile</Button> */}
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                                 {/* <Avatar alt={username} src="/static/images/avatar/2.jpg" /> */}
-                                <AccountMenu username={username}/>
+                                <AccountMenu username={username} userId={userId}/>
                             </IconButton>
                         </Box>
                     </Toolbar>
